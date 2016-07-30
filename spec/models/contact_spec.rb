436f48e3011853c0768e050e884d1e557d9ab50c
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2016-07-30 23:05:57 +0800

    Refactoring 2 tests into 1 block

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -48,21 +48,26 @@ describe Contact do
 		expect(contact.name).to eq "John Doe"
 	end
 
-	it "returns a SORTED array of results that match" do
-		smith 	= Contact.create(firstname: 'John', lastname: 'Smith', email: 'jsmith@example.com')
-		jones 	= Contact.create(firstname: 'Tim', lastname: 'Jones', email: 'tj@example.com')
-		johnson = Contact.create(firstname: 'John', lastname: 'Johnson', email: 'jj@example.com')
+	describe "filter last name by letter" do
+		before :each do
+			@smith 	 = Contact.create(firstname: 'John', lastname: 'Smith', email: 'jsmith@example.com')
+			@jones 	 = Contact.create(firstname: 'Tim', lastname: 'Jones', email: 'tj@example.com')
+			@johnson = Contact.create(firstname: 'John', lastname: 'Johnson', email: 'jj@example.com')
+		end
 
-		# note we’re testing both the results of the query and the sort order;
-		# jones will be retrieved from the database first but since we’re sorting by
-		# last name then johnson should be stored first in the query results
-		expect(Contact.by_letter('J')).to eq [johnson, jones]
-	end
+		context "matching letters" do
+			it "returns a SORTED array of results that match" do
+				# note we’re testing both the results of the query and the sort order;
+				# jones will be retrieved from the database first but since we’re sorting by
+				# last name then johnson should be stored first in the query results
+				expect(Contact.by_letter('J')).to eq [@johnson, @jones]
+			end
+		end
 
-	it "returns a SORTED array of results that match" do
-		smith 	= Contact.create(firstname: 'John', lastname: 'Smith', email: 'jsmith@example.com')
-		jones 	= Contact.create(firstname: 'Tim', lastname: 'Jones', email: 'tj@example.com')
-		johnson = Contact.create(firstname: 'John', lastname: 'Johnson', email: 'jj@example.com')
-		expect(Contact.by_letter('J')).not_to include smith
+		context "non-matching letters" do
+			it "omits results that do not match" do
+				expect(Contact.by_letter('J')).not_to include @smith
+			end
+		end
 	end
 end
